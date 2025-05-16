53811912fc12bd867ba66e2764bce5d564940b04
Juan Gomez <31415575+JuanGomez12@users.noreply.github.com>
2025-05-16 00:03:34 -0400

    Add distance calculator class

--- src/AirportCalculator.py
+++ src/AirportCalculator.py
@@ -1,4 +1,38 @@
+import logging
+from math import atan2, cos, radians, sin, sqrt
+
 import airportsdata
+from vincenty import vincenty
+
+logger = logging.getLogger(__name__)
+logger_format = "[%(asctime)s - %(filename)s:%(lineno)s] - %(funcName)s() - %(levelname)s %(message)s"
+logging.basicConfig(level=logging.INFO, format=logger_format)
+
+
+class DistanceCalculator:
+    @staticmethod
+    def haversine(point1: tuple, point2: tuple) -> float:
+        """
+        Calculate the great-circle distance between two points on the Earth specified in decimal degrees.
+        """
+        # Convert decimal degrees to radians
+        lat1, lon1 = map(radians, point1)
+        lat2, lon2 = map(radians, point2)
+
+        # Haversine formula
+        dlon = lon2 - lon1
+        dlat = lat2 - lat1
+        a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+        c = 2 * atan2(sqrt(a), sqrt(1 - a))
+        r = 6371.0
+        return c * r  # returns distance in kilometers
+
+    @staticmethod
+    def vincenty(point1: tuple, point2: tuple) -> float:
+        """
+        Calculate the distance between two points on the Earth using the Vincenty formula.
+        """
+        return vincenty(point1, point2)
 
 
 class AirportCalculator:
@@ -31,12 +65,19 @@ class AirportCalculator:
         Returns:
             float: The distance between the two airports in kilometers.
         """
+        airport_start = self.get_airport_info(iata_start)
+        airport_end = self.get_airport_info(iata_end)
 
-        if len(iata_end) > 3 or iata_end not in self.airports:
-            raise ValueError(f"Invalid IATA code: {iata_end}")
-        self.get_airport_info(iata_start)
-        self.get_airport_info(iata_end)
-        return 0.0
+        point1 = airport_start["lat"], airport_start["lon"]
+        point2 = airport_end["lat"], airport_end["lon"]
+
+        try:
+            distance = DistanceCalculator.vincenty(point1, point2)
+        except Exception as e:
+            error_message = f"Error calculating distance between {iata_start} and {iata_end} using vincenty: {e}, falling back to haversine"
+            logger.error(error_message)
+            distance = DistanceCalculator.haversine(point1, point2)
+        return distance
 
     def calculate_multipart_distance(self, iata_codes: list) -> float:
         """
@@ -54,3 +95,12 @@ class AirportCalculator:
         for i in range(len(iata_codes) - 1):
             total_distance += self.calculate_distance(iata_codes[i], iata_codes[i + 1])
         return total_distance
+
+
+if __name__ == "__main__":
+    from pprint import pprint
+
+    airport_calculator = AirportCalculator()
+    pprint(airport_calculator.get_airport_info("LAX"))
+    print(airport_calculator.calculate_distance("LAX", "JFK"), "km")
+    print(airport_calculator.calculate_multipart_distance(["LAX", "JFK", "ORD"]), "km")
